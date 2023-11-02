96fe00307b47a73a0b8b860a7134760ede718188
Muhammad Yermi Rachman <146173233+Aroneru@users.noreply.github.com>
2023-11-02 21:28:58 +0700

    Update 1_HelloWorld.py

--- Modular/1_HelloWorld.py
+++ Modular/1_HelloWorld.py
@@ -1,16 +1,10 @@
+# Muhamamad Yermi Rachman
+# J0403231034
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world() -> str:
-    """
-    Display Hello, Flask! on the web page
-
-    Returns:
-        str: Hello, Flask!
-    """
+def hello_world():
     return "<p>Hello, Flask!</p>"
-
-if __name__ == '__main__':
-    app.run()
